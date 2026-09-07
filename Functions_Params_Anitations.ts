4d2960661072340eb29e5a5990c6e3300782a263
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 13:14:25 +0100

    Return Annotation

--- Functions_Params_Anitations.ts
+++ Functions_Params_Anitations.ts
@@ -5,6 +5,7 @@ const add = (num_1: number, num_2: number) => num_1 + num_2;
 
 console.log(add(12, 5));
 
+//Return Annotation
 const checkNumber = (num: number): boolean => num > 5;
 
 console.log(checkNumber(6));
---
+++ Return_Annotation.ts
@@ -0,0 +1,6 @@
+//Return Annotation in TypeScript
+//A return annotation tells TypeScript what type a function is expected to return.
+
+const checkName = (name: string): boolean => name === "John";
+
+console.log(checkName("Doe"));

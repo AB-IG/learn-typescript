13a70e16ccd62ec3ef527f16c90edf86cc541ce3
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 13:21:02 +0100

    Void

---
+++ Void.ts
@@ -0,0 +1,7 @@
+// void in TypeScript
+// void is used for functions that don't return a useful value.
+
+const gretting = (name: string): void =>
+  console.log(`Hello my name is ${name}`);
+
+gretting("John Doe");

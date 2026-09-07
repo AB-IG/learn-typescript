138911d6764805ac96d5013cdfad1e1af28310cd
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 14:32:46 +0100

    multiDimentional Arrays

---
+++ multiDimentionalArray.ts
@@ -0,0 +1,10 @@
+// Multidimensional Arrays in TypeScript
+// A multidimensional array is an array that contains other arrays.
+
+const singleDi: number[] = [1, 2, 3];
+const multiDi: number[][] = [[4, 5, 6]];
+const tripleDi: number[][][] = [[[7, 8, 9]]];
+
+console.log(singleDi);
+console.log(multiDi);
+console.log(tripleDi);

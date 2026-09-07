ed4352cb3fc1d288cffe997ac408541669c7bed1
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 12:34:14 +0100

    Type Inferring

---
+++ Type_Inferring.ts
@@ -0,0 +1,13 @@
+//Type Inferring
+// When you give a variable a value, TypeScript automatically guesses (infers) it's type.
+// You don't need to write it
+
+const username = "John Doe";
+// number
+const user_age = 29;
+// boolean
+const isAdult = true;
+
+console.log(typeof username);
+console.log(typeof user_age);
+console.log(typeof isAdult);

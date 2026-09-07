0f0103b884736dc7f503206efbc783038b543662
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 12:57:36 +0100

    Function Parameter Anotation

---
+++ Functions_Params_Anitations.ts
@@ -0,0 +1,10 @@
+// Function Paramter Anotation
+// In TypeScript, parameter annotations specify the type of values a function expects.
+
+const add = (num_1: number, num_2: number) => num_1 + num_2;
+
+console.log(add(12, 5));
+
+const checkNumber = (num: number): boolean => num > 5;
+
+console.log(checkNumber(6));

3897545f2929db01499daafab752028a60b2af63
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 12:14:53 +0100

    first commit

---
+++ Anotation.ts
@@ -0,0 +1,16 @@
+// String
+let myName: string = "John Doe";
+myName = "John";
+console.log(myName);
+
+//Number
+const age: number = 25;
+const monthlySal: number = 20000;
+
+const annualSal: number = (monthlySal * 25) / 12;
+console.log(annualSal.toFixed());
+
+//Boolean
+const isHard: boolean = false;
+
+console.log("Is typeScript Hard? :", isHard);

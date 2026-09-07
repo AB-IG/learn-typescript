2f4c2819a552d7a46fa2da3db9856570026aa874
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-07 15:13:37 +0100

    Type_Aliases

---
+++ Type_Aliases.ts
@@ -0,0 +1,19 @@
+// Type Aliases in TypeScript
+// A type alias lets you give a custom name to a type. It is created using the type keyword.
+
+type User = {
+  name: string;
+  age: number;
+  location: string;
+};
+
+const personData = (user: User) => {
+  return { name: user.name, age: user.age, location: user.location };
+};
+console.log(personData({ name: "John Doe", age: 30, location: "Abuja" }));
+
+const userData = ({ name, age, location }: User) => {
+  return { name, age, location };
+};
+
+console.log(userData({ name: "Paul", age: 44, location: "Manchester" }));

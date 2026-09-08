6d1b59ea68d6cdc267c59bad8837cf4015f399f0
Abraham Markus <abrahammarkus222@gmail.com>
2026-09-08 08:44:40 +0100

    Tupple

---
+++ Tupple.ts
@@ -0,0 +1,14 @@
+// Tuple in TypeScript
+// A tuple in TypeScript is a special type of array where you specify:
+
+// How many elements it should have
+// What type each element should be
+// The order of those types
+
+const games: [string, string, number] = ["Game 1", "Game 2", 12];
+
+console.log(games);
+
+let person: [string, number];
+
+person = ["John", 25];
